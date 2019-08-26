130d059b3550ecf040c412fffb628b54cf030c58
ticquique <ticquique@gmail.com>
2019-08-26 21:54:34 +0200

    Added retriever

---
+++ retriever.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while getopts ":i:n:m:e:" arg; do
+  case $arg in
+    s) secret=$OPTARG;;
+    i) id=$OPTARG;;
+  esac
+done
+date=`date +"%Y-%m-%d %H:%M:%S"`
+token=`curl -u ${id}:${secret} -d grant_type=client_credentials https://eu.battle.net/oauth/token | jq -j '.access_token'`
+# europe=("shazzrah" "flamelash" "gandling" "razorgore" "firemaw" "gehennas" "golemag" "mograine")
+europe=("tyrande" "exodar")
+for i in "${europe[@]}"; do   # The quotes are necessary here
+    url=`curl --header "Authorization: Bearer ${token}" https://eu.api.blizzard.com/wow/auction/data/$i?locale=en_US | jq -j .files[0].url`
+    curl $url > "auction_${i}_${date}.json"
+done
